ab4a0a0ef9914cbe34548cf7252e6e6662650219
Suhas Patnaik <suhaspatnaik111@gmail.com>
2024-12-18 23:01:05 +0530

    Light & Dark modes set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return "Hello World!";
+  return (
+    <>
+      <p className="">Hello World!</p>
+    </>
+  );
 }
 
 export default App;
