2f8aa2819bf3e5a676699735dd50d522a3ddcbb3
Suhas Patnaik <suhaspatnaik111@gmail.com>
2025-02-02 22:13:39 +0530

    Quiz page: Question + Options added

--- src/components/Page/QuizPage.tsx
+++ src/components/Page/QuizPage.tsx
@@ -1,3 +1,47 @@
-export default function SubjectQuizPage() {
-  return <div>QuizPage</div>;
+import { useParams } from "react-router";
+import { useState } from "react";
+
+import quizData from "../../data/data.json";
+import OptionCard from "../Feature/OptionCard";
+
+export default function QuizPage() {
+  const { subjectName } = useParams();
+
+  const quizIndex = quizData.quizzes.findIndex(
+    (quiz) => quiz.title.toLowerCase() === subjectName?.toLowerCase()
+  );
+
+  const quiz = quizIndex !== -1 ? quizData.quizzes[quizIndex] : null;
+
+  console.log(quiz);
+
+  const [currentQuestionIndex, setCurrentQuestionindex] = useState(0);
+
+  return (
+    <div className="py-8">
+      <h2 className="italic text-light-bluish text-[0.875rem]">
+        Question {currentQuestionIndex + 1} of 10
+      </h2>
+      <QuestionAnswer currentQuestion={quiz?.questions[currentQuestionIndex]} />
+    </div>
+  );
+}
+
+const multipleChoice = ["A", "B", "C", "D"];
+
+function QuestionAnswer({ currentQuestion }) {
+  console.log(currentQuestion);
+  return (
+    <>
+      <p>{currentQuestion.question}</p>
+      {/* <ProgressSlider /> */}
+      {currentQuestion.options.map((option, index) => (
+        <OptionCard
+          key={index}
+          optionLabel={multipleChoice[index]}
+          optionName={option}
+        />
+      ))}
+    </>
+  );
 }
